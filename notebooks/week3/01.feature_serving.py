835fb96f0ba353a3a9c1574a8897066a676ceb55
javedhassans <javedhassans@outlook.com>
2024-11-19 15:25:27 +0100

    created 01.feature_serving.py file

--- notebooks/week3/01.feature_serving.py
+++ notebooks/week3/01.feature_serving.py
@@ -1,14 +1,29 @@
 # Databricks notebook source
 
+# MAGIC %pip install ../childhealth_mlops_with_databricks-0.0.1-py3-none-any.whl
+
+# COMMAND ----------
+
+# MAGIC %restart_python
+
+# COMMAND ----------
+
+
+import random
+import time
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 import mlflow
 import pandas as pd
+import requests
 from databricks import feature_engineering
+from databricks.feature_engineering import FeatureLookup
 from databricks.sdk import WorkspaceClient
 from databricks.sdk.service.catalog import (
     OnlineTableSpec,
     OnlineTableSpecTriggeredSchedulingPolicy,
 )
+from databricks.sdk.service.serving import EndpointCoreConfigInput, ServedEntityInput
 from pyspark.sql import SparkSession
 
 from childHealth.config import ProjectConfig
@@ -80,10 +95,137 @@ spark.sql(f"""
 # 2. Create the online table using feature table
 
 spec = OnlineTableSpec(
-    primary_key_columns=["Id"],
+    primary_key_columns=["id"],
     source_table_full_name=feature_table_name,
     run_triggered=OnlineTableSpecTriggeredSchedulingPolicy.from_dict({"triggered": "true"}),
     perform_full_copy=False,
 )
 
 # COMMAND ----------
+
+# Create the online table in Databricks
+try:
+    online_table_pipeline = workspace.online_tables.get(name=online_table_name)
+except:
+    print("table already exists")
+    pass
+
+# COMMAND ----------
+
+# 3. Create feture look up and feature spec table feature table
+
+# Define features to look up from the feature table
+features = [
+    FeatureLookup(
+        table_name=feature_table_name, lookup_key="id", feature_names=["predicted_sii"]
+    )
+]
+
+# Create the feature spec for serving
+feature_spec_name = f"{catalog_name}.{schema_name}.return_predictions"
+try:
+    fe.create_feature_spec(name=feature_spec_name, features=features, exclude_columns=None)
+except:
+    print("table already exists")
+    pass
+
+# COMMAND ----------
+# 4. Create endpoing using feature spec
+
+try:
+    # Create a serving endpoint for the house prices predictions
+    workspace.serving_endpoints.create(
+        name="child-health-feature-serving",
+        config=EndpointCoreConfigInput(
+            served_entities=[
+                ServedEntityInput(
+                    entity_name=feature_spec_name,  # feature spec name defined in the previous step
+                    scale_to_zero_enabled=True,
+                    workload_size="Small",  # Define the workload size (Small, Medium, Large)
+                )
+            ]
+        ),
+    )
+except:
+    print("endpoint already exists")
+    pass
+
+# COMMAND ----------
+
+token = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().get()
+host = spark.conf.get("spark.databricks.workspaceUrl")
+
+# COMMAND ----------
+
+id_list = preds_df["id"]
+
+# COMMAND ----------
+
+start_time = time.time()
+serving_endpoint = f"https://{host}/serving-endpoints/child-health-feature-serving/invocations"
+response = requests.post(
+    f"{serving_endpoint}",
+    headers={"Authorization": f"Bearer {token}"},
+    json={"dataframe_records": [{"id": "0068a485"}]},
+)
+
+end_time = time.time()
+execution_time = end_time - start_time
+
+print("Response status:", response.status_code)
+print("Reponse text:", response.text)
+print("Execution time:", execution_time, "seconds")
+
+# COMMAND ----------
+# another way to call the endpoint
+
+response = requests.post(
+    f"{serving_endpoint}",
+    headers={"Authorization": f"Bearer {token}"},
+    json={"dataframe_split": {"columns": ["id"], "data": [["0068a485"]]}},
+)
+
+# MAGIC ## Load Test
+
+# COMMAND ----------
+# Initialize variables
+serving_endpoint = f"https://{host}/serving-endpoints/child-health-feature-serving/invocations"
+id_list = preds_df.select("id").rdd.flatMap(lambda x: x).collect()
+headers = {"Authorization": f"Bearer {token}"}
+num_requests = 10
+
+
+# Function to make a request and record latency
+def send_request():
+    random_id = random.choice(id_list)
+    start_time = time.time()
+    response = requests.post(
+        serving_endpoint,
+        headers=headers,
+        json={"dataframe_records": [{"id": random_id}]},
+    )
+    end_time = time.time()
+    latency = end_time - start_time  # Calculate latency for this request
+    return response.status_code, latency
+
+
+# Measure total execution time
+total_start_time = time.time()
+latencies = []
+
+# Send requests concurrently
+with ThreadPoolExecutor(max_workers=100) as executor:
+    futures = [executor.submit(send_request) for _ in range(num_requests)]
+
+    for future in as_completed(futures):
+        status_code, latency = future.result()
+        latencies.append(latency)
+
+total_end_time = time.time()
+total_execution_time = total_end_time - total_start_time
+
+# Calculate the average latency
+average_latency = sum(latencies) / len(latencies)
+
+print("\nTotal execution time:", total_execution_time, "seconds")
+print("Average latency per request:", average_latency, "seconds")
